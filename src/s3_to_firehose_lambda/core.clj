89cad06994c211487c1ca3187ccda3d57187374c
Steen Larsen <sl@skm.dk>
2018-09-19 09:31:19 +0200

    made it more flexible

--- src/s3_to_firehose_lambda/core.clj
+++ src/s3_to_firehose_lambda/core.clj
@@ -10,6 +10,24 @@
             [clj-time.format :as f])
   (:import (java.io ByteArrayInputStream)))
 
+(def s3-bucket (System/getenv "s3_bucket_source"))
+
+(def hose (System/getenv "fh_target"))
+
+(def filter-func (System/getenv "filter_func_name"))
+
+(defn filter1 [m]
+  (prn "M" m)
+  {:vejnavn (get-in m [:vurderingsejendom :adresse :vejnavn])
+   :husnr (get-in m [:vurderingsejendom :adresse :husnr])
+   :postnr (get-in m [:vurderingsejendom :adresse :postnr])
+   :doer (get-in m [:vurderingsejendom :adresse :doer])
+   :etage (get-in m [:vurderingsejendom :adresse :etage])
+   :vurderingsejendomid (get-in m [:vurderingsejendom :vurderingsejendomid])})
+
+(defn filter2 [m]
+  (assoc m :test2 "bar"))
+
 (defn mk-req-handler
   "Makes a request handler"
   [f & [wrt]]
@@ -24,11 +42,16 @@
       (.flush w))))
 
 (defn insert [m]
-  (prn "M" m)
-  (let [bucket "opgave-aggregates"
+  (prn "I" m)
+  (let [bucket s3-bucket
         keys (map #(get-in % [:s3 :object :key]) (:Records m))
-        objects (flatten (map #(line-seq (io/reader (:input-stream (get-object :bucket-name bucket :key %)))) keys))]
-    (prn "O" objects)
-    (mapv #(fh/put-record "opgaver" %) objects)))
+        func (cond
+               (= filter-func "filter1") filter1
+               (= filter-func "filter2") filter2)
+        objects (flatten (mapv #(line-seq (io/reader (:input-stream (get-object :bucket-name bucket :key %)))) keys))
+        _ (prn "O1" objects)
+        filtered-objects (mapv #(func (decode % true)) objects)]
+    (prn "O2" filtered-objects)
+    (mapv #(fh/put-record hose  (encode %)) filtered-objects)))
 
 (def -handleRequest (mk-req-handler insert))
